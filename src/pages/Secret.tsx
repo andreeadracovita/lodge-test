f5711237a427e10f9942273e1ffec07023e0e7e6
Andreea Racovita <andreearacovitagit@gmail.com>
2025-08-30 12:03:10 +0300

    Use features component

--- src/pages/Secret.tsx
+++ src/pages/Secret.tsx
@@ -1,9 +1,12 @@
+import Features from "components/Features";
+
 export default function Secret() {
 	console.log("Rendering Secret Page");
 	
 	return (
 		<>
 			<p>This is a secret page that only an authenticated person can see.</p>
+			<Features />
 		</>
 	);
 }
